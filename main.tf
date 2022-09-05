c20ad567d503f10b1407f00e7daf4cc479dc3c20
mvpc-11232 <mvp2612@hotmail.com>
2022-09-05 08:17:37 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -2,21 +2,13 @@
 #   source = "git::https://github.com/b49-clouddevops/tf-module-vpc.git?ref=main"
 # }
 
-module "vpc" {
-  source           = "./vendor/modules/vpc"
-  VPC_CIDR         = var.VPC_CIDR
-  ENV              = var.ENV
-  AZ               = var.AZ
-  DEFAULT_VPC_ID   = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT   = var.DEFAULT_VPC_RT
-  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  HOSTEDZONE_PRIVATE_ID   =  var.HOSTEDZONE_PRIVATE_ID
-  HOSTEDZONE_PRIVATE_ZONE = var.HOSTEDZONE_PRIVATE_ZONE
-  HOSTEDZONE_PUBLIC_ID    = var.HOSTEDZONE_PUBLIC_ID 
-  HOSTEDZONE_PUBLIC_ZONE  = var.HOSTEDZONE_PUBLIC_ZONE
+module "alb-public" {
+  source           = "./vendor/modules/alb"
+}
 
+
+module "alb-private" {
+  source           = "./vendor/modules/alb"
 }
 
 # You cannot parameterise the source section of the terraform module : Branc name cannot be parameterised. 
